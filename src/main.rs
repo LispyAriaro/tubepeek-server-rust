b1565da8b0678d1b232da165a891759115d739cf
Efe Ariaroo <efeariaroo@gmail.com>
2020-02-26 12:58:41 +0100

    Most recent commit so far. Sorry, more detail never

--- src/main.rs
+++ src/main.rs
@@ -7,66 +7,69 @@ extern crate reqwest;
 extern crate tubepeek_server_rust;
 
 mod db_connection;
-use db_connection::{PgPool, establish_connection};
+use db_connection::{establish_connection, PgPool};
 
 mod ws_dto;
 use ws_dto::*;
-use serde::{Deserialize, Serialize};
 
-use ws::{listen, Handler, Sender, Message, CloseCode};
 use ws::Result as WsResult;
+use ws::{listen, CloseCode, Handler, Message, Sender};
 
 use std::collections::HashMap;
 use std::sync::Mutex;
 
-use serde_json::{Value as JsonValue, Error};
-use diesel::PgConnection;
 use diesel::prelude::*;
+use diesel::PgConnection;
+use serde_json::{json, Error, Value as JsonValue};
 
-use tubepeek_server_rust::models::{Usermaster, NewUser, NewSocialIdentity, SocialIdentity, NewUserFriend};
-use chrono::{Utc, NaiveDateTime};
+use chrono::{NaiveDateTime, Utc};
+use tubepeek_server_rust::models::{
+    NewSocialIdentity, NewUser, NewUserFriend, SocialIdentity, Usermaster,
+};
 // use std::ptr::null;
 // use serde_json::Result as JsResult;
 //use tubepeek_server_rust::schema::{usermaster};
-
 
 // Using lazy static to have a global reference to my connection pool
 // However, I feel that for testing/mocking this won't be great.
 lazy_static! {
     static ref POOL: PgPool = { establish_connection() };
-    static ref WS_CONNECTED_CLIENTS: Mutex<HashMap<u32, WsConnectedClientMetadata>> = Mutex::new(HashMap::new());
+    static ref WS_CONNECTED_CLIENTS: Mutex<HashMap<u32, WsConnectedClientMetadata>> =
+        Mutex::new(HashMap::new());
 }
 
+#[derive(Debug)]
 pub struct WsConnectedClientMetadata {
     pub socketId: u32,
-    pub socket : Sender,
+    pub socket: Sender,
     pub googleUserId: String,
     pub currentVideo: Option<WsConnectedClientCurrentVideo>,
-    pub onlineFriends: Box<Vec<WsOnlineFriend>>
+    pub onlineFriends: Box<Vec<WsOnlineFriend>>,
 }
 
+#[derive(Debug)]
 pub struct WsConnectedClientCurrentVideo {
     pub videoUrl: String,
     pub title: String,
     pub thumbnail_url: String,
 }
 
+#[derive(Debug)]
 pub struct WsOnlineFriend {
     pub socketId: u32,
-    pub socket : Sender,
+    pub socket: Sender,
     pub googleUserId: String,
 }
 
-
 struct WsServer {
-    out: Sender
+    out: Sender,
 }
 
 pub enum WsMessageType {
     TakeMySocialIdentity,
     UserChangedOnlineStatus,
     AddThisPersonToMyFriendsList,
-    ChangedVideo
+    ChangedVideo,
 }
 
 impl Handler for WsServer {
@@ -81,16 +84,22 @@ impl Handler for WsServer {
 
         let json_maybe: Result<JsonValue, Error> = get_json_value();
         if let Err(_err) = json_maybe {
-            return self.out.send("Invalid json value")
+            return self.out.send("Invalid json value");
         }
 
         let pool = POOL.clone();
         let database_connection = pool.get().expect("Failed to get pooled connection");
 
         let response = match json_maybe.unwrap()["messageType"].as_str().unwrap() {
-            "TakeMySocialIdentity" => handle_social_identity(&raw_message, &database_connection, &self.out),
-            "UserChangedOnlineStatus" => handle_user_online_status_change(&raw_message, &database_connection),
-            "AddThisPersonToMyFriendsList" => handle_frend_addition(&raw_message, &database_connection),
+            "TakeMySocialIdentity" => {
+                handle_social_identity(&raw_message, &database_connection, &self.out)
+            }
+            "UserChangedOnlineStatus" => {
+                handle_user_online_status_change(&raw_message, &database_connection)
+            }
+            "AddThisPersonToMyFriendsList" => {
+                handle_frend_addition(&raw_message, &database_connection)
+            }
             "ChangedVideo" => handle_vidoe_change(&raw_message, &database_connection, &self.out),
             _ => "Unknown message type".to_owned(),
         };
@@ -108,23 +117,24 @@ impl Handler for WsServer {
         match conn_metadata_maybe {
             Some(conn_metadata) => {
                 connected_clients.remove(&client_conn_id);
-            },
+            }
             _ => println!("Don't panic"),
         };
         match code {
             CloseCode::Normal => println!("The client is done with the connection."),
-            CloseCode::Away   => println!("The client is leaving the site."),
+            CloseCode::Away => println!("The client is leaving the site."),
             _ => println!("The client encountered an error: {}", reason),
         }
     }
 }
 
-fn handle_social_identity(json : &str, connection: &PgConnection, ws_client: &Sender) -> String {
-    let social_identity_maybe: Result<TakeSocialIdentityMessage, Error> = serde_json::from_str(json);
+fn handle_social_identity(json: &str, connection: &PgConnection, ws_client: &Sender) -> String {
+    let social_identity_maybe: Result<TakeSocialIdentityMessage, Error> =
+        serde_json::from_str(json);
 
-    use tubepeek_server_rust::schema::usermaster::dsl::*;
     use tubepeek_server_rust::schema::social_identities::dsl::*;
     use tubepeek_server_rust::schema::userfriends::dsl::*;
+    use tubepeek_server_rust::schema::usermaster::dsl::*;
 
     match social_identity_maybe {
         Ok(social_identity) => {
@@ -133,47 +143,73 @@ fn handle_social_identity(json : &str, connection: &PgConnection, ws_client: &Se
             let google_user_id = social_identity.authData.googleUserId.as_str();
             let friends = social_identity.friends;
 
-            let save_social_identity = |user_record_id: i64, auth_data_email: &str, prov: String, auth_data: &AuthData, now: &NaiveDateTime|  {
-                let new_social_identity = NewSocialIdentity {
-                    user_id: user_record_id,
-                    provider: prov,
-                    email_address: auth_data_email,
-                    full_name: auth_data.fullName.as_str(),
-                    uid: auth_data.googleUserId.as_str(),
-                    image_url: auth_data.imageUrl.as_str(),
-                    created_at: *now,
+            let save_social_identity =
+                |user_record_id: i64,
+                 auth_data_email: &str,
+                 prov: String,
+                 auth_data: &AuthData,
+                 now: &NaiveDateTime| {
+                    let new_social_identity = NewSocialIdentity {
+                        user_id: user_record_id,
+                        provider: prov,
+                        email_address: auth_data_email,
+                        full_name: auth_data.fullName.as_str(),
+                        uid: auth_data.googleUserId.as_str(),
+                        image_url: auth_data.imageUrl.as_str(),
+                        created_at: *now,
+                    };
+
+                    let new_social_id_db_record = diesel::insert_into(social_identities)
+                        .values(&new_social_identity)
+                        .execute(connection)
+                        .expect("Error saving new social identity");
                 };
 
-                let new_social_id_db_record = diesel::insert_into(social_identities)
-                    .values(&new_social_identity)
-                    .execute(connection)
-                    .expect("Error saving new social identity");
-            };
-
             let existing_user_results = usermaster
-                .filter(tubepeek_server_rust::schema::usermaster::dsl::email_address.eq(&social_identity.authData.emailAddress))
+                .filter(
+                    tubepeek_server_rust::schema::usermaster::dsl::email_address
+                        .eq(&social_identity.authData.emailAddress),
+                )
                 .limit(1)
                 .load::<Usermaster>(connection)
                 .expect("Error loading users");
 
             if existing_user_results.len() > 0 {
                 let existing_social_identity = social_identities
-                    .filter(tubepeek_server_rust::schema::social_identities::dsl::user_id.eq(existing_user_results[0].id)
-                        .and(tubepeek_server_rust::schema::social_identities::dsl::provider.eq(&social_identity.provider)))
+                    .filter(
+                        tubepeek_server_rust::schema::social_identities::dsl::user_id
+                            .eq(existing_user_results[0].id)
+                            .and(
+                                tubepeek_server_rust::schema::social_identities::dsl::provider
+                                    .eq(&social_identity.provider),
+                            ),
+                    )
                     .load::<SocialIdentity>(connection)
                     .expect("Error loading user social identity");
 
-                if(existing_social_identity.len() > 0) {
-                    diesel::update(social_identities.filter(
-                        tubepeek_server_rust::schema::social_identities::dsl::id.eq(existing_social_identity[0].id)))
-                        .set((
-                            tubepeek_server_rust::schema::social_identities::dsl::full_name.eq(&social_identity.authData.fullName),
-                            tubepeek_server_rust::schema::social_identities::dsl::image_url.eq(&social_identity.authData.imageUrl),
-                            tubepeek_server_rust::schema::social_identities::dsl::updated_at.eq(&now))
-                        )
-                        .execute(connection);
+                if (existing_social_identity.len() > 0) {
+                    diesel::update(
+                        social_identities.filter(
+                            tubepeek_server_rust::schema::social_identities::dsl::id
+                                .eq(existing_social_identity[0].id),
+                        ),
+                    )
+                    .set((
+                        tubepeek_server_rust::schema::social_identities::dsl::full_name
+                            .eq(&social_identity.authData.fullName),
+                        tubepeek_server_rust::schema::social_identities::dsl::image_url
+                            .eq(&social_identity.authData.imageUrl),
+                        tubepeek_server_rust::schema::social_identities::dsl::updated_at.eq(&now),
+                    ))
+                    .execute(connection);
                 } else {
-                    save_social_identity(existing_user_results[0].id, auth_data_email, social_identity.provider, &social_identity.authData, &now);
+                    save_social_identity(
+                        existing_user_results[0].id,
+                        auth_data_email,
+                        social_identity.provider,
+                        &social_identity.authData,
+                        &now,
+                    );
                 }
             } else {
                 let new_user = NewUser {
@@ -186,18 +222,30 @@ fn handle_social_identity(json : &str, connection: &PgConnection, ws_client: &Se
                     .get_result::<Usermaster>(connection)
                     .expect("Error saving new user");
 
-                save_social_identity(new_user_db_record.id, auth_data_email, social_identity.provider, &social_identity.authData, &now);
+                save_social_identity(
+                    new_user_db_record.id,
+                    auth_data_email,
+                    social_identity.provider,
+                    &social_identity.authData,
+                    &now,
+                );
             }
 
             for friend_auth in friends.iter() {
                 let existing_friend = userfriends
-                    .filter(tubepeek_server_rust::schema::userfriends::dsl::user_google_uid.eq(google_user_id)
-                        .and(tubepeek_server_rust::schema::userfriends::dsl::friend_google_uid.eq(&friend_auth.googleUserId)))
+                    .filter(
+                        tubepeek_server_rust::schema::userfriends::dsl::user_google_uid
+                            .eq(google_user_id)
+                            .and(
+                                tubepeek_server_rust::schema::userfriends::dsl::friend_google_uid
+                                    .eq(&friend_auth.googleUserId),
+                            ),
+                    )
                     .limit(1)
                     .load::<tubepeek_server_rust::models::UserFriend>(connection)
                     .expect("Error loading user social identity");
 
-                if(existing_friend.len() == 0) {
+                if (existing_friend.len() == 0) {
                     let new_user_friend = NewUserFriend {
                         user_google_uid: google_user_id,
                         friend_google_uid: &friend_auth.googleUserId,
@@ -213,16 +261,19 @@ fn handle_social_identity(json : &str, connection: &PgConnection, ws_client: &Se
             }
 
             let mut connected_clients = WS_CONNECTED_CLIENTS.lock().unwrap();
-            connected_clients.insert(ws_client.connection_id(), WsConnectedClientMetadata {
-                socketId: ws_client.connection_id(),
-                socket: ws_client.to_owned(),
-                googleUserId: google_user_id.to_owned(),
-                currentVideo: None,
-                onlineFriends: Box::new(vec![])
-            });
+            connected_clients.insert(
+                ws_client.connection_id(),
+                WsConnectedClientMetadata {
+                    socketId: ws_client.connection_id(),
+                    socket: ws_client.to_owned(),
+                    googleUserId: google_user_id.to_owned(),
+                    currentVideo: None,
+                    onlineFriends: Box::new(vec![]),
+                },
+            );
 
-            return "all good".to_owned()
-        },
+            return "all good".to_owned();
+        }
         Err(err_msg) => {
             println!("Invalid take social identity.");
             "bad".to_owned()
@@ -232,45 +283,89 @@ fn handle_social_identity(json : &str, connection: &PgConnection, ws_client: &Se
     "All good".to_owned()
 }
 
-fn handle_user_online_status_change(json : &str, connection: &PgConnection) -> String {
+fn handle_user_online_status_change(json: &str, connection: &PgConnection) -> String {
     println!("Got UserChangedOnlineStatus message.");
 
     "All good".to_owned()
 }
 
-fn handle_frend_addition(json : &str, connection: &PgConnection) -> String {
+fn handle_frend_addition(json: &str, connection: &PgConnection) -> String {
     println!("Got AddThisPersonToMyFriendsList message.");
 
     "All good".to_owned()
 }
 
-fn handle_vidoe_change(json : &str, connection: &PgConnection, ws_client: &Sender) -> String {
+fn handle_vidoe_change(json: &str, connection: &PgConnection, ws_client: &Sender) -> String {
     println!("Got ChangedVideo message.");
     let video_change_maybe: Result<VideoChangeMessage, Error> = serde_json::from_str(json);
 
-    use tubepeek_server_rust::schema::usermaster::dsl::*;
     use tubepeek_server_rust::schema::social_identities::dsl::*;
+    use tubepeek_server_rust::schema::usermaster::dsl::*;
 
     match video_change_maybe {
         Ok(video_change) => {
             let video_url = video_change.videoUrl.as_str();
             let google_user_id = video_change.googleUserId.as_str();
-            let youtube_query_url = format!("http://www.youtube.com{}{}", "/oembed?format=json&url=", video_url);
-
+            let youtube_query_url = format!(
+                "http://www.youtube.com{}{}",
+                "/oembed?format=json&url=", video_url
+            );
             println!("youtube_query_url: {}", youtube_query_url);
 
             let youtube_response_maybe = reqwest::blocking::get(youtube_query_url.as_str());
             match youtube_response_maybe {
                 Ok(valid_response) => {
-                    let decoded_video_details = valid_response.json::<YoutubeVideoResponse>();
+                    let decoded_video_details =
+                        valid_response.json::<YoutubeVideoResponse>().unwrap();
+                    let video_title = decoded_video_details.title;
+                    let video_thumbnail = decoded_video_details.thumbnail_url;
 
-                    println!("decoded_video_details {:#?}", decoded_video_details);
-                },
+                    let client_conn_id = ws_client.connection_id();
+
+                    let mut connected_clients = WS_CONNECTED_CLIENTS.lock().unwrap();
+                    let conn_metadata_maybe: Option<&mut WsConnectedClientMetadata> =
+                        connected_clients.get_mut(&client_conn_id);
+
+                    match conn_metadata_maybe {
+                        Some(conn_metadata) => {
+                            conn_metadata.currentVideo = Some(WsConnectedClientCurrentVideo {
+                                videoUrl: video_url.to_string(),
+                                title: video_title.to_string(),
+                                thumbnail_url: video_thumbnail.to_string(),
+                            });
+
+                            // FOR THE LOVE OF GOD, I AM JUST TRYING TO PRINT THIS. BUT BORROW CHECKER IS NOT ALLOWING ME.
+                            // WITH THE LINE COMMENTED OUT, THE PROGRAM RUNS THOUGH.
+                            // println!("connected_clients: {:?}", connected_clients);
+
+                            // connected_clients
+                            //     .entry(client_conn_id)
+                            //     .or_insert(conn_metadata);
+
+                            let broadcast_data = json!({
+                                "action": "TakeFriendVideoChange",
+                                "googleUserId": google_user_id,
+                                "videoData": {
+                                    "videoUrl": video_url,
+                                    "title": video_title,
+                                    "thumbnail_url": video_thumbnail
+                                }
+                            });
+
+                            for friend in conn_metadata.onlineFriends.iter() {
+                                friend.socket.send(broadcast_data.to_string());
+                            }
+                        }
+                        _ => println!("Don't panic"),
+                    };
+
+                    // println!("decoded_video_details {:#?}", &decoded_video_details);
+                }
                 Err(err_msg) => {
                     println!("Invalid video change.");
                 }
             }
-        },
+        }
         Err(err_msg) => {
             println!("Invalid video change.");
         }
@@ -278,9 +373,8 @@ fn handle_vidoe_change(json : &str, connection: &PgConnection, ws_client: &Sende
     "All good".to_owned()
 }
 
-
 fn main() {
     println!("Tubepeek server up and running ...");
 
-    listen("127.0.0.1:9160", |out| WsServer { out: out } ).unwrap()
+    listen("127.0.0.1:9160", |out| WsServer { out: out }).unwrap()
 }
