11980042f81b21b9d285df23b3e556e010aa3af1
Efe Ariaroo <efeariaroo@gmail.com>
2020-03-01 13:38:06 +0100

    Remove unnecessary comment

--- src/utils.rs
+++ src/utils.rs
@@ -3,8 +3,6 @@ use std::string::ToString;
 
 
 // Got regex from the implementation of https://docs.rs/rafy/0.2.1/rafy/
-
-// Use https://gist.github.com/miguelmota/05eac33822d2da5fef1a855b8bc4c3cb for regex replace github gist
 pub fn get_youtube_videoid(videoUrl: &str) -> Option<String> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"^.*(?:(?:youtu\.be/|v/|vi/|u/w/|embed/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*").unwrap();
